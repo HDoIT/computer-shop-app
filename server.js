b69570df8a7f1e8a71c39e220e0bb4ef197e3962
HDoIT <lehuudo2k@gmail.com>
2022-08-16 00:18:15 +0700

    updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(
